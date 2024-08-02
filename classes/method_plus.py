b657947bbcccf3023ac9859cb4952aa1d0e564ac
vagnerasilva <vagnerasilva>
2024-08-01 22:04:45 -0300

    feat: estudo

---
+++ classes/method_plus.py
@@ -0,0 +1,93 @@
+from abc import ABC, abstractmethod
+
+
+class Logistics:
+    def createTransport(self):
+        pass
+
+    def planDelivery(self):
+        transport = self.createTransport()      
+        result = f"Logistics: Transporte sendo preparado... \n{transport.deliver()}"
+        return result
+
+
+class RoadLogistics(Logistics):
+
+    def __init__(self, name):
+        self.name = name
+
+    def createTransport(self):
+        return Truck(self.name)
+
+
+class SeadLogistics(Logistics):
+
+    def __init__(self, name):
+        self.name = name
+
+    def createTransport(self):
+        return Ship(self.name)
+
+
+class FlyLogistics(Logistics):
+
+    def __init__(self, name):
+        self.name = name
+
+    def createTransport(self):
+        return Fly(self.name)
+
+
+class Transport(ABC):
+    @abstractmethod
+    def deliver(self):
+        pass
+
+
+class Truck(Transport):
+    def __init__(self, name):
+        self.name = name
+        self.category = "truck"
+
+    def deliver(self):
+        result = (f"{self.category} preparando para entrega: {self.name}",
+                  "Transporte terreo.... ")
+        return result
+
+
+class Ship(Transport):
+    def __init__(self, name):
+        self.name = name
+        self.category = "ship"
+
+    def deliver(self):
+        result = (f"{self.category} preparando para entrega: {self.name}",
+                  "Transporte maritimo.... ")
+        return result
+
+
+class Fly(Transport):
+    def __init__(self, name):
+        self.name = name
+        self.category = "fly"
+
+    def deliver(self):
+        result = (f"{self.category} preparando para entrega: {self.name}",
+                  "Transporte areo.... ")
+        return result
+
+
+def client_code(logistics: Logistics):
+    print(f"App: Carregando com {logistics.__class__.__name__}.",
+          f"{logistics.planDelivery()}")
+
+
+if __name__ == "__main__":
+    client_code(RoadLogistics("caminhao1"))
+
+    client_code(SeadLogistics("navio"))
+
+    client_code(FlyLogistics("aviao"))
+
+
+## https://youtu.be/CTagrzz-GXs?si=ct92WuQ1tZlSm0WC
