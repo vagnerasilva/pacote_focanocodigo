6b3c9696c97646e8b16e7d16bfef869e3f99bef9
vagnerasilva <vagnerasilva>
2024-08-03 10:32:29 -0300

    feat: ajustes na aula 8

--- estudo/metodos_classe/run.py
+++ estudo/metodos_classe/run.py
@@ -7,24 +7,15 @@ class MinhaClasse:
     def print_variavel_de_classe(self):
         print(self.estatico)
 
-    def alteracao_da_variavel_de_classe(self):
-        self.estatico = "algumacoisa"
-
-
+    @classmethod
+    def alteracao_da_variavel_de_classe(cls):
+        cls.estatico = "algumacoisa"
 
 obj1 = MinhaClasse(True)
 obj2 = MinhaClasse(True)
 
-
-#MinhaClasse.estatico = "programador"
-#MinhaClasse.estatico = "lhamaAqui"
-
-obj1.print_variavel_de_classe()
 obj1.alteracao_da_variavel_de_classe()
-
-
 
 print(obj1.estatico)
 print(obj2.estatico)
-
 print(MinhaClasse.estatico)
