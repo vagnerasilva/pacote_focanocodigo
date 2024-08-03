0995c19504819a894fa4dd0650e4ff94b2419ef8
vagnerasilva <vagnerasilva>
2024-08-02 22:22:32 -0300

    feat: ajustes na pasta estudo e getters_setters

---
+++ estudo/getters_setters/app.py
@@ -0,0 +1,18 @@
+class MinhaClasse:
+    def __init__(self) -> None:
+        self.__valor = None
+        self.__elem = None
+
+    def setter_valor(self, valor: int) -> None:
+        self.__valor = valor
+
+    def getter_valor(self) -> int:
+        return self.__valor
+
+
+my_class = MinhaClasse()
+
+my_class.setter_valor(42)
+valor = my_class.getter_valor()
+
+print(valor)
---
+++ estudo/getters_setters/readme.md
@@ -0,0 +1,4 @@
+![alt text](image.png)
+
+
+https://www.youtube.com/watch?v=AF_OrvHpqlU
